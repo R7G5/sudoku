289203270f7374ec4762a8210941e38486f14e41
Riad Guliyev <riad.guliyev@gmail.com>
2019-11-29 15:25:28 -0500

    Added twinList attribute dict and SetTwinList() method to keep track of unique possibilities for complex puzzles

--- sudoku3.py
+++ sudoku3.py
@@ -9,7 +9,7 @@ class Cell:
         self.row = row                      # cell row position
         self.col = col                      # cell column position
         self.exceptions = []                # list of values not allowed in cell due to position
-        self.possibilities = []             # list of allows possible values
+        self.possibilities = []             # list of possible values
 
 class Puzzle:
 
@@ -20,12 +20,23 @@ class Puzzle:
         self.CurrentBoard = puzzle.copy()   # simple array representation of the board
         self.board.append([])               # add first primary board
         self.moves = []
+        self.twinList = {}                  # list of identical possibilities per row or col.
+                                            # key = tuple of possible numbers
+                                            # value = list of touple coordinates
+
+        '''
+        mydict = {
+            possibilities : ((row1,col1),...)
+	        (2,4,7) : (0,1),
+	        (5,8) : ((1,4),(3,0))
+	        }
+        '''
 
         for i in range(0,9):
             self.board[self.index].append([])   # create new row
             for j in range (0,9):
-                self.board[self.index][i].append(Cell(i,j,puzzle[i][j]))
-                self.board[self.index][i][j].solved = puzzle[i][j] != 0
+                self.board[self.index][i].append(Cell(i,j,puzzle[i][j]))        # copy the value to the cell
+                self.board[self.index][i][j].solved = puzzle[i][j] != 0         # Marked solved is not 0
         self.setAllSpecials()
 
     def getCurrentBoard(self):
@@ -71,23 +82,38 @@ class Puzzle:
 
     def getExceptions(self, row, col):
         tmp = []
-        tmp = set(self.getBlock(row, col) + self.getCol(col) + self.getRow(row))
-        tmp = [elem for elem in tmp if elem != 0]  # remove all zeros
+        tmp = set(self.getBlock(row, col) + self.getCol(col) + self.getRow(row))    # create unique list of possibilities
+        tmp = [elem for elem in tmp if elem != 0]                                   # remove all zeros
         return tmp
 
     def getPossibleNumbers(self, row, col):
         allNumbers =  [1, 2, 3, 4, 5, 6, 7, 8, 9]
         exceptionList = self.getExceptions(row, col)
-        return list(set(allNumbers) - set(exceptionList))
+        return list(set(allNumbers) - set(exceptionList))   # list of numbers not in exception list
 
     def setAllSpecials(self):
         for i in range(0, 9):
             for j in range (0,9):
-                self.board[self.index][i][j].exceptions = self.getExceptions(i, j)
-                self.board[self.index][i][j].possibilities = self.getPossibleNumbers(i, j)
+                if not self.board[self.index][i][j].solved:
+                    self.board[self.index][i][j].exceptions = self.getExceptions(i, j)
+                    self.board[self.index][i][j].possibilities = self.getPossibleNumbers(i, j)
+
+
+    # Populates attribute twinList with identical possibiities
+    def seTwinList(self):
+        self.twinList = {}  # clear existing data
+
+        for i in range(0, 9):
+            for j in range (0,9):
+                if not self.board[self.index][i][j].solved:
+                    tpl = tuple(self.board[self.index][i][j].possibilities)     # get list of current possibilities
+                    if tpl not in self.twinList.keys():                         # if does not exist
+                        self.twinList[tpl] = []                                 # create one
+                    self.twinList[tpl].append((i,j))                            # add (row,col) tuple to the list
+    #return ?
 
     def SetAllSinglePossibilities(self):
-        #count = []
+
         for i in range(0, 9):
             for j in range (0,9):
                 if ( (not self.board[self.index][i][j].solved)) and (len(self.board[self.index][i][j].possibilities) == 1):
@@ -97,12 +123,13 @@ class Puzzle:
 
                     self.moves.append({"row":i, "col":j, "value_before":old_Value, "value_after": new_Value })
 
-                    self.board[self.index][i][j].value =  new_Value  # set the only possibility as a value
-                    self.CurrentBoard[i][j] = new_Value # maintain simple board copy
+                    self.board[self.index][i][j].value =  new_Value     # set the only possibility as a value
+                    self.board[self.index][i][j].solved = True          # mark cell solved
+                    self.CurrentBoard[i][j] = new_Value                 # maintain simple board copy
 
                     self.board[self.index][i][j].solved = True
                     self.setAllSpecials()  # recalculate all exceptions and possibilities
-                    #count.append((i,j))
+
         tmp = copy.deepcopy(self.CurrentBoard)
         return tmp
 
@@ -146,7 +173,7 @@ class Puzzle:
 
 
 # main module
-
+'''
 # simple one
 my_puzzle = [[7, 0, 5, 2, 0, 0, 0, 0, 4],
              [0, 8, 0, 0, 0, 0, 7, 0, 0],
@@ -169,7 +196,7 @@ my_puzzle = [[1, 0, 4, 0, 0, 8, 0, 0, 6],
              [0, 0, 0, 0, 3, 0, 0, 0, 0],
              [4, 0, 0, 6, 0, 0, 1, 0, 7]]
 
-'''
+
 
 myPuz = Puzzle(my_puzzle)                       # create puzzle
 time_start = time.time()                        # get current time
@@ -180,8 +207,21 @@ if (solved):
     print("Sudoku is solved in %s seconds,  and %s placements" % (time_dlt , len(myPuz.moves)))
 else:
     print("Sudoku is still UNRESOLVED in %s seconds,  and %s placements" % (time_dlt , len(myPuz.moves)))
+    myPuz.seTwinList()
 
 print("The End.")
 
+# ToDo:
+#   [ ] Create twinList - list of identical possibilities for the board
+#   [ ] Collect list of cells with identical possibility list information in the same row or column
+#       [ ]
+#       [ ] if yes,
+#           [ ] for each number in possibility list
+#               [ ] copy puzzle with only that possibility number and try to solve it
+#           [ ] if still not solved
+#               [ ] return possibilities back to the list of available possibilities
+#               [ ] try next
+#
 # currently solves simple puzzles
 # need to explore more options
+
